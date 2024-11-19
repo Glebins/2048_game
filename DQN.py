8ae44896dd2b941a9e78e1b8dea898c39da95cff
glebins <nedobuga.gd@gmail.com>
2024-11-20 01:21:28 +0300

    DQN is done

--- DQN.py
+++ DQN.py
@@ -14,44 +14,46 @@ from torchrl.data import ReplayBuffer
 class Q(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(Q, self).__init__()
-        self.embed = nn.Embedding(16, 32)
-        self.conv1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=2, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=2, out_channels=4, kernel_size=3, stride=1, padding=1)
 
-        self.fc1 = nn.Linear(128 * 5 * 5, 256)
-        self.fc2 = nn.Linear(256, 64)
-        self.fc3 = nn.Linear(64, 4)
+        self.fc1 = nn.Linear(4 * 4 * 4, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, 4)
 
-    def handle_input(self, x):
-        print(x.shape)
-        x_hat = copy.deepcopy(x)
-        x_hat[x_hat == 0] += 1
-        x_hat = np.log2(x_hat)
-        x_hat /= torch.max(x_hat)
-        return x_hat
+        # self.linear_1 = nn.Linear(16, 40)
+        # self.linear_2 = nn.Linear(40, 4)
 
     def forward(self, x):
-        x = torch.log2(x).clamp(min=0).long()  # Tile values like 2, 4, 8, etc. become 1, 2, 3
-        x = self.embed(x)
+        if x.ndim == 1:
+            x = x.reshape(4, 4)
+        else:
+            x = x.reshape(-1, 4, 4)
+        x = torch.log2(x).clamp(min=0).float()
+        x = x / x.max()
 
         if x.ndim == 2:
-            x = x[None, :]
+            x = x[None, None, :]
+        elif x.ndim == 3:
+            x = x.reshape(x.shape[0], 1, x.shape[1], x.shape[2])
+        else:
+            raise ValueError(f"Strange x shape: {x.ndim} dimensions (should be 2 or 3), "
+                             f"shape = {x.shape}")
 
-        x = x.permute(0, 2, 1)
-        x = F.relu(self.conv1(x))  # Shape: (batch_size, 64, 5, 5)
-        x = F.relu(self.conv2(x))  # Shape: (batch_size, 128, 5, 5)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
 
-        # Flatten the output
-        x = x.view(x.size(0), -1)  # Shape: (batch_size, 128 * 5 * 5)
+        x = x.reshape(x.shape[0], -1)
 
-        # Fully connected layers
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         q_vals = self.fc3(x)
 
-        # x = self.handle_input(x)
-        # x = F.relu(self.linear1(x.float()))
-        # q_vals = self.linear2(x)
+        # x[x == 0] += 1
+        # x = torch.log2(x)
+        #
+        # x = F.relu(self.linear_1(x))
+        # q_vals = self.linear_2(x)
         return q_vals
 
 
@@ -67,10 +69,12 @@ class DQN:
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
         self.rng = np.random.default_rng()
 
-    def action(self, state, soft=0):
+    def actions_by_probability(self, state, soft=0):
         if self.rng.random() < soft:
-            return self.rng.choice(self.actions)
-        return self.actions[torch.argmax(self.model(state)).item()]
+            # return self.rng.choice(self.actions)
+            return torch.Tensor(self.rng.choice([0, 1, 2, 3], 4, replace=False))
+        # return self.actions[torch.argmax(self.model(state)).item()]
+        return torch.argsort(-self.model(state))[0]
 
     def add_exp(self, state, action, reward, new_state, terminated):
         self.buffer.extend([[state, self.action_to_ind[action], reward, new_state, np.int32(terminated)]])
@@ -88,54 +92,71 @@ class DQN:
 
     def test(self, env):
         state, _ = env.reset()
-        action = self.action(torch.Tensor(state))
+        action = self.actions_by_probability(torch.Tensor(state))
         state, reward, terminated, truncated, info = env.step(action)
         total_reward = reward
         while not(terminated or truncated):
-            action = self.action(torch.Tensor(state))
+            action = self.actions_by_probability(torch.Tensor(state))
             state, reward, terminated, truncated, info = env.step(action)
             total_reward += reward
         return total_reward
 
-    def test_2048(self, game, repeats=100):
+    def test_2048(self, game, repeats=100, penalty=0):
         game_copy = copy.deepcopy(game)
 
         rewards = []
+        number_of_steps = []
 
-        for i in range(repeats):
+        for rep in range(repeats):
             game_copy.init()
             state = game_copy.get_state()
 
-            action = self.action(torch.Tensor(state))
+            actions = self.actions_by_probability(torch.Tensor(state)).detach().numpy()
             action_to_do = {0: 'Left', 1: 'Down', 2: 'Right', 3: 'Up'}
 
+            was_positions_changed = False
+            terminated = False
             prev_score = game_copy.score
-            was_positions_changed = game_copy.do_move(action_to_do[action])
-            state = game_copy.get_state()
-            total_reward = game_copy.score - prev_score
-            terminated = game_copy.is_game_over
+            i = 0
+            total_reward = 0
 
-            counter_bad_moves = int(not was_positions_changed)
-            truncated = counter_bad_moves >= 10
-
-            while not (terminated or truncated):
-                action = self.action(torch.Tensor(state))
-
-                prev_score = game_copy.score
-                was_positions_changed = game_copy.do_move(action_to_do[action])
-                state = game_copy.get_state()
-                reward = game_copy.score - prev_score
+            while not was_positions_changed:
+                was_positions_changed = game_copy.do_move(action_to_do[actions[i]])
+                total_reward = game_copy.score - prev_score
                 terminated = game_copy.is_game_over
 
-                total_reward += reward
+                if not was_positions_changed:
+                    total_reward += penalty
+                i += 1
 
-                if was_positions_changed:
-                    counter_bad_moves = 0
+            number_of_steps.append(1)
 
-                counter_bad_moves += int(not was_positions_changed)
-                truncated = counter_bad_moves >= 10
+            state = game_copy.get_state()
 
+            while not terminated:
+                actions = self.actions_by_probability(torch.Tensor(state)).detach().numpy()
+
+                was_positions_changed = False
+                i = 0
+                prev_score = game_copy.score
+
+                while not was_positions_changed:
+                    was_positions_changed = game_copy.do_move(action_to_do[actions[i]])
+                    reward = game_copy.score - prev_score
+
+                    if not was_positions_changed:
+                        reward = penalty
+
+                    terminated = game_copy.is_game_over
+                    total_reward += reward
+                    i += 1
+
+                number_of_steps[rep] += 1
+
+                state = game_copy.get_state()
+
+            number_of_steps[rep] -= 1
             rewards.append(total_reward)
 
-        return rewards
+        return rewards, np.array(number_of_steps)
 
--- main.py
+++ main.py
@@ -9,49 +9,55 @@ from game_draw import *
 
 game = Game2048()
 
-step = 0
-counter_bad_moves = 0
+step = -1
 game.init()
 state = game.get_state()
 
-model = DQN([0, 1, 2, 3], in_dim=16, gamma=1, lr=0.01)
+model = DQN([0, 1, 2, 3], in_dim=16, gamma=1, lr=1)
+
+# model.model.load_state_dict(torch.load(f"model_mlp_2700_0.pth"))
 
 amount_of_epochs = 100_000_000
 
 for epoch in range(amount_of_epochs):
     step += 1
-    action = model.action(torch.Tensor(state), 0.1)
+    actions = model.actions_by_probability(torch.Tensor(state), 0).detach().numpy()
 
     action_to_do = {0: 'Left', 1: 'Down', 2: 'Right', 3: 'Up'}
 
+    was_positions_changed = False
     prev_score = game.score
-    was_positions_changed = game.do_move(action_to_do[action])
-    new_state = game.get_state()
-    reward = game.score - prev_score
-    terminated = game.is_game_over
 
-    counter_bad_moves += int(not was_positions_changed)
+    i = 0
+    terminated = False
 
-    if was_positions_changed:
-        counter_bad_moves = 0
+    while not was_positions_changed:
+        was_positions_changed = game.do_move(action_to_do[actions[i]])
 
-    # if not was_positions_changed:
-    #     reward = 0.9 * reward
+        new_state = game.get_state()
+        reward = game.score - prev_score
+        terminated = game.is_game_over
 
-    truncated = counter_bad_moves >= 10
+        if not was_positions_changed:
+            reward = -100
 
-    model.add_exp(state, action, reward, new_state, terminated)
-    state = new_state
+        model.add_exp(state, actions[i], reward, new_state, terminated)
+        i += 1
 
-    if terminated or truncated:
+    state = game.get_state()
+
+    if terminated:
         game.init()
         counter_bad_moves = 0
 
     if step > 100:
         model.update()
 
-    if step % 10_000 == 0:
-        test_rewards = model.test_2048(game, 100)
-        check_mean = sum(test_rewards) / len(test_rewards)
-        print(f"{step}: {check_mean}")
+    if step % 2_000 == 0:
+        test_rewards, num_steps = np.array(model.test_2048(game, repeats=100, penalty=0))
+        print(f"{step}: {test_rewards.mean()},\tmedian = {np.median(test_rewards)},"
+              f"\tmin = {test_rewards.min()},\tmax = {test_rewards.max()},"
+              f"\tsteps mean = {num_steps.mean()}")
+        if test_rewards.mean() > 3100:
+            torch.save(model.model.state_dict(), f"model_mlp_2700_1.pth")
 
